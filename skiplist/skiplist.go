15bf2028a633f5fda3520f2113a982b02aa015a8
jianjun <justmao945@gmail.com>
2015-08-12 19:04:05 +0800

    improve

--- skiplist/skiplist.go
+++ skiplist/skiplist.go
@@ -16,17 +16,27 @@ var (
 	MaxLevel = 32
 )
 
-type node struct {
-	key     interface{}
-	value   interface{}
-	forward []*node
+// Element represents a node in skip list.
+type Element struct {
+	Key     interface{}
+	Value   interface{}
+	forward []*Element // len(forward) is in [1, MaxLevel]
 }
 
-func newNode(key, value interface{}, level int) *node {
-	return &node{
-		key:     key,
-		value:   value,
-		forward: make([]*node, level+1),
+func newElement(key, value interface{}, level int) *Element {
+	return &Element{
+		Key:     key,
+		Value:   value,
+		forward: make([]*Element, level+1),
+	}
+}
+
+// Next iterate next element in skip list.
+func (e *Element) Next() *Element {
+	if e != nil {
+		return e.forward[0]
+	} else {
+		return nil
 	}
 }
 
@@ -37,16 +47,18 @@ type SkipList struct {
 	// if a is less than b.
 	Less func(a, b interface{}) bool
 
-	level int
-	head  *node
+	level int      // [0, MaxLevel)
+	head  *Element // forward size is MaxLevel
 	rand  *rand.Rand
 }
 
+// Creates a new SkipList with the compare function less.
+// Function less is used to compare keys to insert.
 func New(less func(a, b interface{}) bool) *SkipList {
 	return &SkipList{
 		Less:  less,
 		level: 0,
-		head:  newNode(nil, nil, MaxLevel-1),
+		head:  newElement(nil, nil, MaxLevel-1),
 		rand:  rand.New(rand.NewSource(time.Now().Unix())),
 	}
 }
@@ -74,89 +86,76 @@ func (s *SkipList) equal(a, b interface{}) bool {
 
 // Find element in table and save previous pointer which
 // needs to be modified on all levels
-func (s *SkipList) find(key interface{}, update []*node) *node {
+func (s *SkipList) find(key interface{}, update []*Element) *Element {
 	cur := s.head
+	// go from level top to bottom
 	for i := s.level; i >= 0; i-- {
-		for i < len(cur.forward) && cur.forward[i] != nil && s.Less(cur.forward[i].key, key) {
+		// go from left to right until equal or greater than key
+		for i < len(cur.forward) && cur.forward[i] != nil && s.Less(cur.forward[i].Key, key) {
 			cur = cur.forward[i]
 		}
 		if update != nil {
+			// save the point that goes down
 			update[i] = cur
 		}
 	}
+	// the node that found, or stopped when not found
 	return cur.forward[0]
 }
 
-func (s *SkipList) layout() string {
-	var buf bytes.Buffer
-	buf.WriteByte('\n')
-	for i := 0; i < s.level; i++ {
-		buf.WriteString(fmt.Sprintf("%d: ", i))
-		for cur := s.head.forward[i]; cur != nil; cur = cur.forward[i] {
-			if cur != s.head.forward[i] {
-				buf.WriteString(", ")
-			}
-			buf.WriteString(fmt.Sprintf("%p:%v:%v", cur, cur.key, cur.value))
-		}
-		buf.WriteByte('\n')
-	}
-	return buf.String()
-}
-
-func (s *SkipList) String() string {
-	var buf bytes.Buffer
-	buf.WriteByte('{')
-	for cur := s.head.forward[0]; cur != nil; cur = cur.forward[0] {
-		if cur != s.head.forward[0] {
-			buf.WriteString(", ")
-		}
-		buf.WriteString(fmt.Sprintf("%v:%v", cur.key, cur.value))
-	}
-	buf.WriteByte('}')
-	return buf.String()
-}
-
-func (s *SkipList) Insert(key, value interface{}) bool {
-	update := make([]*node, MaxLevel)
+// Insert returns the new element and sets ok to true if added successfully,
+// otherwise returns the duplicated one and sets ok to false.
+func (s *SkipList) Insert(key, value interface{}) (e *Element, ok bool) {
+	update := make([]*Element, MaxLevel)
 	cur := s.find(key, update)
 
-	if cur != nil && s.equal(cur.key, key) {
-		return false
+	if cur != nil && s.equal(cur.Key, key) {
+		e = cur
+		ok = false
+		return
 	}
 
 	level := s.randomLevel()
+	// may have several new levels
 	if level > s.level {
 		for i := s.level + 1; i <= level; i++ {
+			// need save to link the new node
 			update[i] = s.head
 		}
 		s.level = level
 	}
 
-	cur = newNode(key, value, level)
+	cur = newElement(key, value, level)
+	e = cur
+	ok = true
 
+	// insert the new node after all nodes in update[] for all levels
 	for i := 0; i <= level; i++ {
 		cur.forward[i] = update[i].forward[i]
 		update[i].forward[i] = cur
 	}
 
-	return true
+	return
 }
 
+// Remove returns false if key is not in skip list.
 func (s *SkipList) Remove(key interface{}) bool {
-	update := make([]*node, MaxLevel)
+	update := make([]*Element, MaxLevel)
 	cur := s.find(key, update)
 
-	if !s.equal(cur.key, key) {
+	if !s.equal(cur.Key, key) {
 		return false
 	}
 
 	for i := 0; i <= s.level; i++ {
 		if update[i].forward[i] != cur {
-			break
+			break // stop when hit the top
 		}
+		// remove current node
 		update[i].forward[i] = cur.forward[i]
 	}
 
+	// shrink level if has no element
 	for s.level > 0 && s.head.forward[s.level] == nil {
 		s.level--
 	}
@@ -164,10 +163,11 @@ func (s *SkipList) Remove(key interface{}) bool {
 	return true
 }
 
-func (s *SkipList) Find(key interface{}) (value interface{}, ok bool) {
-	cur := s.find(key, nil)
-	if cur != nil && s.equal(cur.key, key) {
-		value = cur.value
+// Find returns the element and sets ok to true if key is in the skip list,
+// otherwise sets ok to false.
+func (s *SkipList) Find(key interface{}) (e *Element, ok bool) {
+	e = s.find(key, nil)
+	if e != nil && s.equal(e.Key, key) {
 		ok = true
 	} else {
 		ok = false
@@ -175,7 +175,50 @@ func (s *SkipList) Find(key interface{}) (value interface{}, ok bool) {
 	return
 }
 
+// Contains test whether the key is in the list or not.
 func (s *SkipList) Contains(key interface{}) bool {
 	_, ok := s.Find(key)
 	return ok
+}
+
+func (s *SkipList) Len() int {
+	i := 0
+	for p := s.Front(); p != nil; p = p.Next() {
+		i++
+	}
+	return i
+}
+
+func (s *SkipList) Front() *Element {
+	return s.head.forward[0]
+}
+
+func (s *SkipList) layout() string {
+	var buf bytes.Buffer
+	for i := 0; i < s.level; i++ {
+		buf.WriteString(fmt.Sprintf("%d: ", i))
+		for cur := s.head.forward[i]; cur != nil; cur = cur.forward[i] {
+			if cur != s.head.forward[i] {
+				buf.WriteString(", ")
+			}
+			buf.WriteString(fmt.Sprintf("%p:%v:%v", cur, cur.Key, cur.Value))
+		}
+		buf.WriteByte('\n')
+	}
+	return buf.String()
+}
+
+// String returns the representation of the entire list:
+//	 {k1:v1, k2:v2, ...}
+func (s *SkipList) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for cur := s.Front(); cur != nil; cur = cur.Next() {
+		if cur != s.Front() {
+			buf.WriteString(", ")
+		}
+		buf.WriteString(fmt.Sprintf("%v:%v", cur.Key, cur.Value))
+	}
+	buf.WriteByte('}')
+	return buf.String()
 }
--- skiplist/skiplist_test.go
+++ skiplist/skiplist_test.go
@@ -10,13 +10,44 @@ func TestSkipList(t *testing.T) {
 	l := New(func(a, b interface{}) bool {
 		return a.(int) < b.(int)
 	})
-	t.Log(l.layout())
+	t.Logf("empty:\n%+v\n%s", l, l.layout())
 
+	// test insert
 	for _, v := range elems {
-		l.Insert(v, v+10000)
+		e, ok := l.Insert(v, v+10000)
+		if !ok {
+			t.Error(v)
+		}
+		if e.Key.(int) != v {
+			t.Error(v)
+		}
+		if e.Value.(int) != v+10000 {
+			t.Error(v)
+		}
 	}
-	t.Log(l.layout())
+	t.Logf("inserted:\n%+v\n%s", l, l.layout())
 
+	// test insert dup
+	e, ok := l.Insert(100, 10100)
+	if ok {
+		t.Error(100)
+	}
+
+	e1, ok := l.Find(100)
+	if !ok {
+		t.Error(100)
+	}
+	if e1 != e {
+		t.Error(100)
+	}
+
+	// test String()
+	t.Logf("string:\n", l)
+	if l.String() != "{0:10000, 1:10001, 2:10002, 3:10003, 4:10004, 5:10005, 7:10007, 10:10010, 22:10022, 56:10056, 68:10068, 77:10077, 100:10100}" {
+		t.Error(l)
+	}
+
+	// test find
 	for i := 0; i < 200; i++ {
 		has := false
 		for _, v := range elems {
@@ -28,7 +59,31 @@ func TestSkipList(t *testing.T) {
 		if has != l.Contains(i) {
 			t.Error(i)
 		}
+		e, ok := l.Find(i)
+		if ok {
+			if !has {
+				t.Error(i)
+			}
+			if e.Key.(int) != i {
+				t.Error(i)
+			}
+			if e.Value.(int) != i+10000 {
+				t.Error(i)
+			}
+		}
 	}
 
-	//elems2 := []elem{1, 2, 3, 4, 100}
+	// test len next
+	if len(elems) != l.Len() {
+		t.Error(l.Len())
+	}
+
+	// test remove
+	for _, v := range elems {
+		l.Remove(v)
+		t.Logf("remove %d:\n%+v\n%s", v, l, l.layout())
+		if l.Contains(v) {
+			t.Error(v)
+		}
+	}
 }
